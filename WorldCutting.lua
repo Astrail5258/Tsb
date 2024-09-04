8c6d9f2980d5337bdd78e4cf159210019a6630db
Astrail5258 <120449933+Astrail5258@users.noreply.github.com>
2024-09-04 20:05:33 +0300

    Create WorldCutting.lua

---
+++ WorldCutting.lua
@@ -0,0 +1,19 @@
+local player = game.Players.LocalPlayer
+repeat wait() until player.Character
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoid = character:WaitForChild("Humanoid")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+-- Define the messages you want to send
+local messages = {"SCALE OF THE DRAGON.", "RECOIL.", "TWIN METEORS.", "WORLD CUTTING SLASH!"}
+
+-- Function to send messages to the chat
+local function sendMessage(text)
+    ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(text, "All")
+end
+
+-- Send each message one time with a 1-second delay
+for _, message in ipairs(messages) do
+    sendMessage(message)
+    wait(2) -- Wait 2 seconds before sending the next message
+end
