658ad4cd2affd5701da69b80389fa6fc9344be64
Astrail5258 <120449933+Astrail5258@users.noreply.github.com>
2024-09-09 20:21:42 +0300

    Update MainLoad.lua

--- MainLoad.lua
+++ MainLoad.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Astrail5258/Tsb/Scripts/TheStrongestHeroToItadoriYuji.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Astrail5258/Tsb/Scripts/Load2.lua'))()
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 local humanoid = player.Character.Humanoid
