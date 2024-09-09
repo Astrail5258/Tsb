786cf65b06d0720e3a787c2c22f66d5ddfa1fa21
Astrail5258 <120449933+Astrail5258@users.noreply.github.com>
2024-09-09 20:21:11 +0300

    Create Load2.lua

---
+++ Load2.lua
@@ -0,0 +1,5 @@
+loadstring(game:HttpGet('loadstring(game:HttpGet('https://raw.githubusercontent.com/Astrail5258/Tsb/Scripts/TheStrongestHeroToItadoriYuji.lua'))()'))()
+
+task.wait(2)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Astrail5258/Tsb/Scripts/WorldCutting.lua"))()
