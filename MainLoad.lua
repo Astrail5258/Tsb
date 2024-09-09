5f0ce4a3d3f6a05f8eee696090f5daaf5719dd9c
Astrail5258 <120449933+Astrail5258@users.noreply.github.com>
2024-09-09 20:19:18 +0300

    Create MainLoad.lua

---
+++ MainLoad.lua
@@ -0,0 +1,8 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Astrail5258/Tsb/Scripts/TheStrongestHeroToItadoriYuji.lua'))()
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+local humanoid = player.Character.Humanoid
+while true do
+    humanoid.AutoRotate = true
+    wait(0)
+end
