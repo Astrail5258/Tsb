79f44700967c6c5e15d8dc447fdf02f75712a267
Astrail5258 <120449933+Astrail5258@users.noreply.github.com>
2024-12-18 08:12:06 +0300

    Update HakariMoveset.lua

--- HakariMoveset.lua
+++ HakariMoveset.lua
@@ -1,1 +1,7 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Astrail5258/Tsb/refs/heads/Scripts/tatsumommytohakaritoolrun.lua"))()
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Astrail5258/Tsb/refs/heads/Scripts/tatsumommytohakaritptoolbehind.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Astrail5258/Tsb/refs/heads/Scripts/tatsumommytohakariclicktptool.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Astrail5258/Tsb/refs/heads/Scripts/tatsumommytohakari.lua"))()
