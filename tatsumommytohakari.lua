b40512c351c22aa0eee477db27fa43d4935b88e3
Astrail5258 <120449933+Astrail5258@users.noreply.github.com>
2024-12-18 04:42:28 +0300

    Update tatsumommytohakari.lua

--- tatsumommytohakari.lua
+++ tatsumommytohakari.lua
@@ -590,7 +590,7 @@ Anim.TimePosition = startTime
 
 Anim:AdjustSpeed(1)
 
-wait(7)
+wait(10)
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Astrail5258/Tsb/refs/heads/Scripts/tatsumommytohakariult.lua"))()
 
